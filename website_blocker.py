4cd2f9713f350d9cf1453b1463b2f5fd7384f534
Yusuf Quddus <yusuf_quddus@yahoo.com>
2020-04-02 02:11:12 -0700

    take website list from file

--- website_blocker.py
+++ website_blocker.py
@@ -1,14 +1,20 @@
 import time
 from datetime import datetime as dt
 
-host_paths = "/etc/hosts"
+hosts_path = "/etc/hosts"
 redirect = "127.0.0.1"
-website_list=["www.facebook.com", "facebook.com", "www.reddit.com", "reddit.com"]
+website_list = []
+
+# Take website list from file
+with open("website_list.txt") as file:
+    for line in file:
+        split = line.split()[0]
+        website_list.append(split)
 
 while True:
     # range from 8am - 4pm
     if dt(dt.now().year, dt.now().month, dt.now().day, 8) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 16):
-        print("Working hours")
+        print("Working hours...")
         with open(hosts_path, 'r+') as file:
             content = file.read()
             for website in website_list:
@@ -25,6 +31,6 @@ while True:
                 if not any(website in line for website in website_list):
                     file.write(line)
             file.truncate()
-        print("Fun hours")
+        print("Fun hours...")
 
     time.sleep(5)
